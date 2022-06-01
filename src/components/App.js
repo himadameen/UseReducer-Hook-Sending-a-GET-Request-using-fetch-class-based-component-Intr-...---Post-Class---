b6e34b9dd256eb26b813498369f9c12358eb85af
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-06-01 19:18:12 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
+import React, { useReducer } from "react";
+import { counterReducer } from "../reducers/counterReducer";
+import "../styles/App.css";
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  const initialValues = {
+    count: 0
+  };
+
+  const [state, dispatch] = useReducer(counterReducer, initialValues);
   return (
     <div id="main">
-
+      <span id="counter">{state.count}</span>
+      <button id="increment-btn" onClick={() => dispatch({ type: "INCREASE" })}>
+        Increase
+      </button>
+      <button id="decrement-btn" onClick={() => dispatch({ type: "DECREASE" })}>
+        Decrease
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,16 @@
-const counterReducer = (state,action) =>{
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return {
+        count: state.count + 1
+      };
+    case "DECREASE":
+      return {
+        count: state.count !== 0 ? state.count - 1 : (state.count = 0)
+      };
+    default:
+      return state;
+  }
+};
 
-}
-
-export {counterReducer}
+export { counterReducer };
